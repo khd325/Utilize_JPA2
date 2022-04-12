b67ec419956527c1f2ec4e05c902b8d8bf679984
khd325 <hdk325@naver.com>
2022-04-12 20:46:40 +0900

    3.컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -10,10 +10,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestParam;
-=======
->>>>>>> origin/master
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -52,28 +49,21 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
-
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
-
         return result;
     }
 
     @GetMapping("/api/v3.1/orders")
-<<<<<<< HEAD
+
     public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
                                         @RequestParam(value = "limit", defaultValue = "100") int limit) {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,limit);
-=======
-    public List<OrderDto> ordersV3_page() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
->>>>>>> origin/master
-
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,
+                limit);
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
-
         return result;
     }
 
